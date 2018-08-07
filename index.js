866fb489be5cae18e8f1c12072ea0fd56ddf1281
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-07 19:26:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser='ABCD XYZ';
+
+let welcomeMessage = 'Welcome to Flatbook, '+ currentUser + '!';
+
+let excitedWelcomeMessage='WELCOME TO FLATBOOK, ' + currentUser + '!';
+
+let shortGreeting='Welcome, ' + currentUser.substring(0,1) + '!';
